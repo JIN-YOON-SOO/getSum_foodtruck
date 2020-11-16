e24c2e7199dfedf683e0e333e359f3999d593d6c
bobaejeon <bbj5608@gmail.com>
2020-11-17 01:30:04 +0900

    판매자- 영업여부/위치 설정, 이미지업로드

---
+++ app/src/main/java/com/example/getsumfoot/GPSTracker.java
@@ -0,0 +1,144 @@
+package com.example.getsumfoot;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.util.Log;
+import android.widget.Toast;
+
+//import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import static android.content.Context.LOCATION_SERVICE;
+
+public class GPSTracker implements LocationListener{
+    Location location;
+    Context context;
+    double latitude;
+    double longitude;
+
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1;
+    protected LocationManager locationManager;
+
+
+    public GPSTracker(Context context) {
+        this.context = context;
+        getLocation();
+    }
+
+
+    public Location getLocation() {
+        try {
+            locationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
+
+            boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!isGPSEnabled && !isNetworkEnabled) {
+
+            } else {
+                int hasFineLocationPermission = ContextCompat.checkSelfPermission(context,
+                        Manifest.permission.ACCESS_FINE_LOCATION);
+                int hasCoarseLocationPermission = ContextCompat.checkSelfPermission(context,
+                        Manifest.permission.ACCESS_COARSE_LOCATION);
+
+                if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
+                        hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED) {
+                } else return null;
+
+                if (isNetworkEnabled) {
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, (LocationListener) this);
+
+                    if (locationManager != null) {
+                        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                        }
+                    }
+                }
+
+
+                if (isGPSEnabled) {
+                    if (location == null) {
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, (LocationListener) this);
+                        if (locationManager != null) {
+                            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (location != null) {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Log.e("GPSTracker", ""+e.toString());
+        }
+        return location;
+    }
+
+    public String getAddress(){
+        Geocoder geocoder = new Geocoder(this.context, Locale.getDefault());
+        List<Address> addresses;
+
+        try {
+            addresses = geocoder.getFromLocation(this.latitude, this.longitude, 7);
+        } catch (IOException ioException) { //네트워크 문제
+            Toast.makeText(this.context, "지오코더 서비스 사용불가", Toast.LENGTH_LONG).show();
+            return "지오코더 서비스 사용불가";
+        } catch (IllegalArgumentException illegalArgumentException) {
+            Toast.makeText(this.context, "잘못된 GPS 좌표", Toast.LENGTH_LONG).show();
+            return "잘못된 GPS 좌표";
+        }
+
+        if (addresses == null || addresses.size() == 0) {
+            Toast.makeText(this.context, "주소 미발견", Toast.LENGTH_LONG).show();
+            return "주소 미발견";
+        }
+
+        Address address = addresses.get(0);
+        return address.getAddressLine(0).toString();
+    }
+
+    public double getLatitude() {
+        if(location != null) {
+            latitude = location.getLatitude();
+        }
+        return latitude;
+    }
+
+    public double getLongitude() {
+        if(location != null) {
+            longitude = location.getLongitude();
+        }
+        return longitude;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) { }
+
+    @Override
+    public void onProviderDisabled(String provider) { }
+
+    @Override
+    public void onProviderEnabled(String provider) { }
+
+    public void stopUsingGPS() {
+        if(locationManager != null) {
+            locationManager.removeUpdates(GPSTracker.this);
+        }
+    }
+}
--- app/src/main/java/com/example/getsumfoot/data/ImageData.java
+++ app/src/main/java/com/example/getsumfoot/data/ImageData.java
@@ -1,19 +1,33 @@
 package com.example.getsumfoot.data;
 
+import java.util.HashMap;
+
 public class ImageData {
     public String image_uri;
     public String image_id;
 
     public ImageData(){}
-    public ImageData(String image_uri, String image_id){
+    public ImageData(String image_uri, String uid){
         this.image_uri = image_uri;
-        this.image_id = image_id;
+        this.image_id = uid + "_" + System.currentTimeMillis();
     }
 
-    public String getImage_id() {
+    public String getImageId() {
         return image_id;
     }
-    public String getImage_uri(){
+    public void setImageId(String image_id){
+        this.image_id = image_id;
+    }
+    public String getImageUri(){
         return image_uri;
+    }
+    public void setImageUri(String image_uri){
+        this.image_uri = image_uri;
+    }
+    public HashMap<String, Object> getImageHash(){
+        HashMap<String, Object> hash = new HashMap<>();
+        hash.put("image_uri", this.image_uri);
+        hash.put("image_id", this.image_id);
+        return hash;
     }
 }
